84d1421655e64d906bc101a49a4864f7474f3df2
faizaaulia <faizaaulia20@gmail.com>
2020-05-28 13:51:06 +0700

    finish convert from html template add router-link

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="breadcrumb-text product-more text-left">
-                            <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+                            <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
                             <span>Detail</span>
                         </div>
                     </div>
@@ -75,7 +75,9 @@
                                         <h4>$495.00</h4>
                                     </div>
                                     <div class="quantity">
-                                        <a href="shopping-cart.html" class="primary-btn pd-cart">Add To Cart</a>
+                                        <router-link to="/cart">
+                                            <a href="#" class="primary-btn pd-cart">Add To Cart</a>
+                                        </router-link>
                                     </div>
                                 </div>
                             </div>
@@ -85,6 +87,7 @@
             </div>
         </section>
         <!-- Product Shop Section End -->
+        <RelatedShayna/>
         <FooterShayna />
     </div>
 </template>
@@ -93,6 +96,7 @@
     // @ is an alias to /src
     import HeaderShayna from '@/components/HeaderShayna.vue'
     import FooterShayna from '@/components/FooterShayna.vue'
+    import RelatedShayna from '@/components/RelatedShayna.vue'
     import carousel from 'vue-owl-carousel'
 
     export default {
@@ -100,6 +104,7 @@
         components: {
             HeaderShayna,
             FooterShayna,
+            RelatedShayna,
             carousel
         },
         data() {
