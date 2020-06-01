65282fe79bb880295c1e64770522b77b76d2d0ad
faizaaulia <faizaaulia20@gmail.com>
2020-06-01 21:39:21 +0700

    API integration pt. 1

--- src/components/BannerShayna.vue
+++ src/components/BannerShayna.vue
@@ -4,9 +4,9 @@
         <section class="women-banner spad">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-12 mt-5">
+                    <div class="col-lg-12 mt-5" v-if="products.length > 0">
                         <carousel class="product-slider" :nav='false' :item='3' :autoplay='true' :dots='false'>
-                            <div class="product-item">
+                            <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
                                 <div class="pi-pic">
                                     <img src="img/products/women-2.jpg" alt="" />
                                     <ul>
@@ -19,82 +19,19 @@
                                     </ul>
                                 </div>
                                 <div class="pi-text">
-                                    <div class="catagory-name">Shoes</div>
+                                    <div class="catagory-name">{{ itemProduct.type }}</div>
                                     <router-link to="/product">
-                                        <h5>Guangzhou sweater</h5>
+                                        <h5>{{ itemProduct.name }}</h5>
                                     </router-link>
                                     <div class="product-price">
-                                        $13.00
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-3.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Towel</div>
-                                    <a href="#">
-                                        <h5>Pure Pineapple</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $34.00
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-4.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                        <li class="w-icon">
-                                            <a href="#"><i class="fa fa-random"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Towel</div>
-                                    <a href="#">
-                                        <h5>Converse Shoes</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $34.00
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-item">
-                                <div class="pi-pic">
-                                    <img src="img/products/women-1.jpg" alt="" />
-                                    <ul>
-                                        <li class="w-icon active">
-                                            <a href="#"><i class="icon_bag_alt"></i></a>
-                                        </li>
-                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                        <li class="w-icon">
-                                            <a href="#"><i class="fa fa-random"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="pi-text">
-                                    <div class="catagory-name">Towel</div>
-                                    <a href="#">
-                                        <h5>Converse Shoes</h5>
-                                    </a>
-                                    <div class="product-price">
-                                        $34.00
+                                        ${{ itemProduct.price }}
                                     </div>
                                 </div>
                             </div>
                         </carousel>
+                    </div>
+                    <div class="col-lg-12 mt-5" v-else>
+                        <p>Produk belum tersedia untuk saat ini</p>
                     </div>
                 </div>
             </div>
@@ -105,11 +42,23 @@
 
 <script>
 import carousel from 'vue-owl-carousel'
+import axios from 'axios'
 
 export default {
     name: 'BannerShayna',
     components: {
         carousel
+    },
+    data() {
+        return {
+            products: []
+        };
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:8000/api/products')
+            .then(res => (this.products = res.data.data.data))
+            .catch(err => console.log(err));
     }
 }
 </script>
